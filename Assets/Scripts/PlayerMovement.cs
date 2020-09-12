992f027683a146719ab4e528c1b502c1768cf21e
batophobia <godrox3@yahoo.com>
2020-09-12 00:43:00 -0500

    Initial Commit

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+  public float speed = 6f;
+  public float turnSpeed = .1f;
+
+  CharacterController controller;
+  Camera cam;
+  float turnVelocity;
+
+  void Start() {
+    controller = GetComponent<CharacterController>();
+    cam = Camera.main;
+  }
+
+  void Update() {
+    float horiz = Input.GetAxisRaw("Horizontal");
+    float vert = Input.GetAxisRaw("Vertical");
+
+    Vector3 dir = new Vector3(horiz, 0, vert).normalized;
+
+    if(dir.magnitude >= 0.1f){
+      float targetAngle = Mathf.Atan2(dir.x, dir.z) * Mathf.Rad2Deg + cam.transform.eulerAngles.y;
+      float angle =Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnVelocity, turnSpeed);
+      transform.rotation = Quaternion.Euler(0, angle, 0);
+
+      Vector3 moveDir = Quaternion.Euler(0, targetAngle, 0) * Vector3.forward;
+      controller.Move(moveDir.normalized * speed * Time.deltaTime);
+    }
+  }
+}
